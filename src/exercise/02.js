7278e1f743aca98dd42e7a4d7b302328ce592949
Amanda Herschleb <amanda.herschleb@bonus.ly>
2022-03-09 14:18:17 -0700

    exercise 2 useCallback for memoization

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -28,7 +28,7 @@ function asyncReducer(state, action) {
   }
 }
 
-function useAsync(asyncCallback, initialState, dependencies) {
+function useAsync(asyncCallback, initialState) {
   const [state, dispatch] = React.useReducer(asyncReducer, {
     status: 'idle',
     data: null,
@@ -51,20 +51,26 @@ function useAsync(asyncCallback, initialState, dependencies) {
         dispatch({type: 'rejected', error})
       },
     )  
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, dependencies)
+  }, [asyncCallback])
 
   return state
 }
 
 function PokemonInfo({pokemonName}) {
-  // use the new useAsync hook here
-  const state = useAsync(() => {
+  // better to have useCallback so dependencies can be handled properly
+  // this function definition will only change wehn pokemonName changes
+  // not the best solution but better than the last
+  const asyncCallback = React.useCallback(() => {
     if (!pokemonName) {
       return
     }
     return fetchPokemon(pokemonName)
-  }, {status: pokemonName ? 'pending' : 'idle'}, [pokemonName])
+  }, [pokemonName])
+
+  const state = useAsync(
+    asyncCallback, 
+    {status: pokemonName ? 'pending' : 'idle'}, 
+  )
 
   const {data, status, error} = state
 
