64b0e37ef789105d343cb79844b2c402c7c3f90b
Amanda Herschleb <amanda.herschleb@bonus.ly>
2022-03-09 10:38:15 -0700

    exercise 1 use dispatch object like redux

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -5,21 +5,33 @@ import * as React from 'react'
 
   // The 1st argument is called "state" - the current value of count
   // The 2nd argument is called "action" - a function that returns the new value of state OR object with new state
+// function countReducer(state, action) {
+//   // returns new value of state
+//   return {
+//     ...state,
+//     ...(typeof action === 'function' ? action(state) : action)
+//   }
+// }
+
+// make it work like redux
 function countReducer(state, action) {
-  // returns new value of state
-  return {
-    ...state,
-    ...(typeof action === 'function' ? action(state) : action)
+  switch (action.type) {
+    case 'INCREMENT': {
+      return {count: state.count + action.step}
+    }
+    default: {
+      throw new Error(`Unsuppored action type: ${action.type}`)
+    }
   }
 }
 
 function Counter({initialCount = 0, step = 2}) {
-  const [state, setState] = React.useReducer(countReducer, {
+  // use dispatch like redux
+  const [state, dispatch] = React.useReducer(countReducer, {
     count: initialCount,
   })
   const {count} = state
-  const increment = () =>
-    setState(currentState => ({count: currentState.count + step}))
+  const increment = () => dispatch({type: 'INCREMENT', step})
 
   return <button onClick={increment}>{count}</button>
 }
